49f10c6ca513012aaacb0940159c60718c66fe6e
Tarak311 <tarak_1@live.com>
2018-07-30 02:06:55 -0400

    indented

--- recipes-i2ctemperature/i2ctemp/files/i2ctemp.c
+++ recipes-i2ctemperature/i2ctemp/files/i2ctemp.c
@@ -7,37 +7,31 @@ int main (void)
 {
 	int deviceHandle;
 	int readBytes;
-char buffer[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-	// initialize buffer
-	buffer[0] = 0x55;
-	// address of DS1307 RTC device
-int deviceI2CAddress = 0x5d;
-	// open device on /dev/i2c-0
-	if ((deviceHandle = open("/dev/i2c-1", O_RDWR)) < 0) {
+  char buffer[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+  buffer[0] = 0x55;
+  int deviceI2CAddress = 0x5d;
+	if ((deviceHandle = open("/dev/i2c-1", O_RDWR)) < 0)
+  {
 		printf("Error: Couldn't open device! %d\n", deviceHandle);
 		return 1;
 	}
-	if (ioctl(deviceHandle, I2C_SLAVE, deviceI2CAddress) < 0) {
+	if (ioctl(deviceHandle, I2C_SLAVE, deviceI2CAddress) < 0)
+    {
 			printf("Error: Couldn't find device on address!\n");
 
 		}
-	// begin transmission and request acknowledgement
-	//readBytes = write(deviceHandle, buffer, 1);
-	//if (readBytes != 1)
-	//{
-	//	printf(readBytes);
-//	}
-	readBytes = read(deviceHandle, buffer, 7);
-	if (readBytes != 7)
-		{
-			printf("Error: Received no data!");
-		}
-		else {	printf(" Received  data!");}
-		printf("value is %02x\n",buffer[2] );
-		printf("value is %02x\n",buffer[3] );
-		printf("value is %02x\n",buffer[1] );
-		printf("value is %02x\n",buffer[0] );
-	// close connection and return
-	close(deviceHandle);
-	return 0;
+
+ //readBytes = write(deviceHandle, buffer, 1);
+ readBytes = read(deviceHandle, buffer, 7);
+ if (readBytes != 7)
+	{
+		printf("Error: Received no data!");
+	}
+ else {	printf(" Received  data!");}
+ printf("value is %02x\n",buffer[0] );
+ printf("value is %02x\n",buffer[1] );
+ printf("value is %02x\n",buffer[2] );
+ printf("value is %02x\n",buffer[3] );
+ close(deviceHandle);
+ return 0;
 }
