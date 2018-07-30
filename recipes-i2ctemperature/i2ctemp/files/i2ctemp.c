2ff782e2511ddfa521cf705816c3a4e36b7c1ad2
Tarak311 <tarak_1@live.com>
2018-07-30 03:41:08 -0400

    Change 1.0 - defined each value

--- recipes-i2ctemperature/i2ctemp/files/i2ctemp.c
+++ recipes-i2ctemperature/i2ctemp/files/i2ctemp.c
@@ -7,7 +7,7 @@ int main (void)
 {
 	int deviceHandle;
 	int readBytes;
-  char buffer[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+  char buffer[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
   buffer[0] = 0x55;
   int deviceI2CAddress = 0x5d;
 	if ((deviceHandle = open("/dev/i2c-1", O_RDWR)) < 0)
@@ -22,16 +22,21 @@ int main (void)
 		}
 
  //readBytes = write(deviceHandle, buffer, 1);
- readBytes = read(deviceHandle, buffer, 7);
+ readBytes = read(deviceHandle, buffer, 9);
  if (readBytes != 7)
 	{
 		printf("Error: Received no data!");
 	}
  else {	printf(" Received  data!");}
- printf("value is %02x\n",buffer[0] );
- printf("value is %02x\n",buffer[1] );
- printf("value is %02x\n",buffer[2] );
- printf("value is %02x\n",buffer[3] );
+ printf("temperature is:      %02x\n",buffer[1] );
+ printf("temperature is:      %02x\n",buffer[0] );
+ printf("High temperature is: %02x\n",buffer[2] );
+ printf("Low temperature is:  %02x\n",buffer[3] );
+ printf("configuration is:    %02x\n",buffer[4] );
+ printf("value is:            %02x\n",buffer[5] );
+ printf("value is:            %02x\n",buffer[6] );
+ printf("value is:            %02x\n",buffer[7] );
+ printf("crc is:            %02x\n",buffer[8] );
  close(deviceHandle);
  return 0;
 }
