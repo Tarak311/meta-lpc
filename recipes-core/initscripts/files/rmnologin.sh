fa1bd38d1f30aaad648b3e56096752b90da68dc7
Tarak311 <tarak_1@live.com>
2018-08-03 11:16:10 -0400

    backup

--- recipes-core/initscripts/files/rmnologin.sh
+++ recipes-core/initscripts/files/rmnologin.sh
@@ -12,11 +12,16 @@
 #                    If DELAYLOGIN=no, /etc/nologin was not created by
 #                    bootmisc earlier in the boot process.
 ### END INIT INFO
-
+IPADDRESS0=192.168.2.40
+IPADDRESS1=192.168.40.188
+IPGATEWAY0=192.168.2.1
+IPGATEWAY1=192.168.40.1
 # This starts our network
 ifconfig wlan0  up                                        
 wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant.conf&           
-ifconfig wlan0 192.168.2.40   
+ifconfig wlan0 $IPADDRESS0   
+route add default gw $IPGATEWAY0
+echo "nameserver 8.8.8.8">>/etc/resolv.conf
 
 #This will setup out i2c-dev
 modprobe i2c-dev
